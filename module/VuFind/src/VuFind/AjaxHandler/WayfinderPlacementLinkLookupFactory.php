d6df275c65d7afe65859a268ac9fcfbd2467be49
OIOIOOOI <anatolii.mn@gmail.com>
2023-06-27 12:20:56 +0300

    EW-66 - Comments correction.

--- module/VuFind/src/VuFind/AjaxHandler/WayfinderPlacementLinkLookupFactory.php
+++ module/VuFind/src/VuFind/AjaxHandler/WayfinderPlacementLinkLookupFactory.php
@@ -1,30 +1,15 @@
 <?php
 
 /**
- * Factory for DoiLookup AJAX handler.
+ * Factory for AJAX handler to lookup Wayfinder placement link.
  *
  * PHP version 8
  *
- * Copyright (C) Villanova University 2018.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * @category VuFind
+ * @category Wayfinder
  * @package  AJAX
- * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Inlead <support@inlead.dk>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://vufind.org/wiki/development Wiki
+ * @link     https://inlead.dk
  */
 
 namespace VuFind\AjaxHandler;
@@ -36,13 +21,15 @@ use Psr\Container\ContainerExceptionInterface as ContainerException;
 use Psr\Container\ContainerInterface;
 
 /**
- * Factory for DoiLookup AJAX handler.
+ * Factory for AJAX handler to lookup Wayfinder placement link.
  *
- * @category VuFind
+ * PHP version 8
+ *
+ * @category Wayfinder
  * @package  AJAX
- * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Inlead <support@inlead.dk>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://vufind.org/wiki/development Wiki
+ * @link     https://inlead.dk
  */
 class WayfinderPlacementLinkLookupFactory implements \Laminas\ServiceManager\Factory\FactoryInterface
 {
